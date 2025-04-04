2ded7913db78ea3712d209ac870ed164b5cf0550
Chaitanya <chaitanya.lal@publicissapient.com>
2025-04-04 23:43:18 +0530

    newFeature

--- firstCode.java
+++ firstCode.java
@@ -4,7 +4,10 @@
  
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Chaitaya");
-        System.out.println("chits");
+        System.out.println("hi");
+
+        for(int i=0;i<10;i++){
+            System.out.println("hello");
+        }
     }
  }
